def4d084c8c7bd5a05fc11d6bb6b77ee46f9e8a0
lenovo <dgon1976@mail.ru>
2023-01-12 21:20:50 +0500

    Пример решения задачи 2

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -3,14 +3,25 @@ package com.example.demo;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
+        task1();
+        task2();
+    }
 
+    public static void task1() {
         String[] strings = {"aa", "aa", "1", "bbb", "aa", "abc", "cc", "abc", "aa", "aa", "dd", "aa", "aa", "bb", "aa", "cc", "aa", "aa", "dd"};
         Set<String> result = new LinkedHashSet<>(List.of(strings));
         System.out.println(result);
+    }
+
+    public static void task2() {
+        Stream.of("aa", "aa", "1", "bbb", "aa", "abc", "cc", "abc", "aa", "aa", "dd")
+                .distinct() // distinct() — метод исключает из стрима повторяющиеся элементы
+                .forEach(System.out::println);
     }
 
 }
